448623c73d9237efcd734c09a3b80e71aaa95dd1
Juan Diego Melo <jdmelo002@gmail.com>
2022-03-05 11:18:08 -0500

    Fix cors

--- app/app.go
+++ app/app.go
@@ -2,16 +2,17 @@ package app
 
 import (
 	"auth-service/app/config"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"os"
 )
 
 func Run() {
 	r := gin.Default()
+	r.Use(cors.Default())
 
 	g := r.Group("/api/v1")
 	config.DefinePaths(g)
-	config.SetCors(g)
 	config.SetLogger(g)
 
 	r.Run(os.Getenv("port"))
