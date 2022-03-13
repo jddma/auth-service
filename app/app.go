c3eca384ce472c7c6d0497d4d6e09fd9b6988eb9
Juan Melo <jdmelo002@gmail.com>
2022-03-13 13:53:43 -0500

    Agregar cors definidos

--- app/app.go
+++ app/app.go
@@ -2,14 +2,13 @@ package app
 
 import (
 	"auth-service/app/config"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"os"
 )
 
 func Run() {
 	r := gin.Default()
-	r.Use(cors.Default())
+	r.Use(config.GetCors())
 
 	g := r.Group("/api/v1")
 	config.DefinePaths(g)
--- app/config/config.go
+++ app/config/config.go
@@ -2,12 +2,20 @@ package config
 
 import (
 	"auth-service/app/controllers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func DefinePaths(router *gin.RouterGroup) {
 	router.POST("/token", controllers.GenerateTokenController)
 	router.POST("/validate-token", controllers.ValidateTokenController)
+}
+
+func GetCors() gin.HandlerFunc {
+	config := cors.DefaultConfig()
+	config.AddAllowHeaders("uuid", "Authorization")
+	config.AllowAllOrigins = true
+	return cors.New(config)
 }
 
 func SetLogger(router *gin.RouterGroup) {
